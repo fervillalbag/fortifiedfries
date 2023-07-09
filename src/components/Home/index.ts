1b030275b762c8b24d2d87096530f29caf7c3ec9
Fernando Villalba <21fervillalbag@gmail.com>
2023-07-09 18:22:24 -0400

    feat: header image home ui

---
+++ src/components/Home/index.ts
@@ -0,0 +1,1 @@
+export { default as HeaderGallery } from "./HeaderGallery";
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { HeaderGallery } from "../components/Home";
+
+const headerImages = [
+  {
+    id: "1",
+    image: "https://shorturl.at/xKRW3",
+  },
+  {
+    id: "2",
+    image: "https://shorturl.at/nxyV1",
+  },
+  {
+    id: "3",
+    image: "https://shorturl.at/FPX01",
+  },
+  {
+    id: "4",
+    image: "https://shorturl.at/abkpX",
+  },
+];
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="p-5">
+        <HeaderGallery data={headerImages} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
