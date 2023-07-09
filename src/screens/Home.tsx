bada6affab256898d0552a3bef8ee91a469b6874
Fernando Villalba <21fervillalbag@gmail.com>
2023-07-09 18:32:56 -0400

    feat: category buttons home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+
 import { HeaderGallery } from "../components/Home";
+import { Button, buttonVariants } from "../ui";
+import { WindowSizeContext } from "../context";
 
 const headerImages = [
   {
@@ -20,11 +23,59 @@ const headerImages = [
   },
 ];
 
+const categories = [
+  {
+    id: "1",
+    text: "Todos",
+  },
+  {
+    id: "2",
+    text: "Informatica",
+  },
+  {
+    id: "3",
+    text: "Muebles",
+  },
+  {
+    id: "4",
+    text: "Prendas",
+  },
+];
+
 const Home: React.FC = () => {
+  const [categorySelected, setCategorySelected] =
+    useState<string>("1");
+  const { windowSize } = useContext(WindowSizeContext);
+
   return (
     <div>
       <div className="p-5">
         <HeaderGallery data={headerImages} />
+      </div>
+
+      <div
+        className={`flex w-[${
+          windowSize - 20
+        }px] overflow-x-auto pl-5 gap-4 hide-scrollbar`}
+      >
+        {categories.map((category, index: number) => (
+          <Button
+            key={category.id}
+            onClick={() => setCategorySelected(category.id)}
+            className={buttonVariants({
+              variant: `${
+                category.id === categorySelected
+                  ? "default"
+                  : "outline"
+              }`,
+              className: `flex-1 px-7 ${
+                index === categories.length - 1 ? "mr-5" : "mr-0"
+              }`,
+            })}
+          >
+            {category.text}
+          </Button>
+        ))}
       </div>
     </div>
   );
