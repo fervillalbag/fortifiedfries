e93324f3966da2b3ca802de3ac76fcf1b8d6b172
Fernando Villalba <21fervillalbag@gmail.com>
2023-08-07 13:56:01 -0400

    feat: fetch fullname and username in profile view and line loader component ui

--- src/screens/Profile/Root.tsx
+++ src/screens/Profile/Root.tsx
@@ -1,9 +1,39 @@
-import { Layout } from "../../components";
-import SettingIcon from "../../assets/icons/settings-icon.svg";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import { useLocalStorageState } from "../../hooks";
+import { LineLoader } from "../../components/Loader";
+import { Layout } from "../../components";
+import { NURA_AUTH_USER_INFO } from "../../utils/constants/auth";
+import { client } from "../../../supabase/client";
+
+import SettingIcon from "../../assets/icons/settings-icon.svg";
+import AvatarDefault from "../../assets/images/avatar-default.png";
+import VerifiedIcon from "../../assets/icons/verified-icon.svg";
 
 export default function Root() {
   const navigate = useNavigate();
+  const [userInfo, setInfoUser] = useState<any>(null);
+
+  const [initialValues] = useLocalStorageState({
+    key: NURA_AUTH_USER_INFO,
+  });
+
+  const getUser = async () => {
+    const response = await client
+      .from("User")
+      .select("*")
+      .eq("email", initialValues.email)
+      .single();
+
+    setInfoUser(response?.data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  console.log({ userInfo });
 
   return (
     <Layout>
@@ -14,6 +44,42 @@ export default function Root() {
         >
           <img src={SettingIcon} alt="" />
         </button>
+      </div>
+
+      <div className="relative">
+        <div className="absolute left-5 -top-[50px]">
+          <img
+            src={AvatarDefault}
+            alt=""
+            className="w-[100px] h-[100px] object-cover rounded-full"
+          />
+
+          <div className="mt-2">
+            {!userInfo?.fullname ? (
+              <LineLoader />
+            ) : (
+              <div className="flex items-center gap-1 ">
+                <p className="text-xl font-medium text-@sura-primary-900">
+                  {userInfo?.fullname ? (
+                    userInfo?.fullname
+                  ) : (
+                    <div></div>
+                  )}
+                </p>
+                <div>
+                  <img src={VerifiedIcon} alt="" />
+                </div>
+              </div>
+            )}
+            {!userInfo?.username ? (
+              <LineLoader />
+            ) : (
+              <p className="text-@sura-primary-400">
+                @{userInfo?.username}
+              </p>
+            )}
+          </div>
+        </div>
       </div>
     </Layout>
   );
