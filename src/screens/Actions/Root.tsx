da3848c287d26477b1f56584ebfc979678aeb46b
Fernando Villalba <21fervillalbag@gmail.com>
2023-08-09 10:24:37 -0400

    fix: actions screen ui delete back button and add layout navbar

--- src/screens/Actions/Root.tsx
+++ src/screens/Actions/Root.tsx
@@ -1,26 +1,31 @@
 import { useNavigate } from "react-router-dom";
 
-import { BackButton } from "../../components";
+import { Layout } from "../../components";
 import { ACTIONS } from "../../../data/actions";
 import { ButtonAction } from "../../components/Actions";
+import { Text } from "../../ui";
 
 export default function Root() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <BackButton title="Acciones" />
+    <Layout>
+      <div className="p-5">
+        <Text className="text-2xl text-@sura-primary-900">
+          Acciones
+        </Text>
 
-      <div className="px-5">
-        {ACTIONS.map((action) => (
-          <ButtonAction
-            key={action.id}
-            title={action.title}
-            description={action.description}
-            onClick={() => navigate(action.route)}
-          />
-        ))}
+        <div className="mt-4">
+          {ACTIONS.map((action) => (
+            <ButtonAction
+              key={action.id}
+              title={action.title}
+              description={action.description}
+              onClick={() => navigate(action.route)}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
