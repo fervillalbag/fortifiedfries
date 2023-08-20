4faa2d5eab687748f53ec901f27170f691b7d012
Fernando Villalba <21fervillalbag@gmail.com>
2023-08-20 03:36:23 -0400

    fix: navbar opacity and hidden

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,9 +15,9 @@ export default function Navbar({ isVisible }: NavbarProps) {
     <div
       className={`z-20 bottom-0 left-0 fixed w-full ${
         isVisible && pathname === "/home"
-          ? "opacity-0"
-          : "opacity-100"
-      } h-[70px] bg-[#f5f5f5] transition-opacity duration-200`}
+          ? "opacity-0 overflow-hidden h-0"
+          : "opacity-100 block overflow-visible h-[70px]"
+      } bg-[#f5f5f5] transition-opacity duration-300`}
     >
       <div className="shadow-md flex items-center h-full justify-between px-8">
         {NAVBAR_ROUTES.map((link) => (
