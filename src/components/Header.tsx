8d181d6c98a7b57e0be05ff29d0d6c3c4bd74ae9
Fernando Villalba <21fervillalbag@gmail.com>
2023-09-01 23:39:49 -0400

    fix: icon profile header and height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
 
   return (
     <div
-      className={`p-5 py-4 sticky top-0 z-50 ${
+      className={`p-5 py-3 sticky top-0 z-50 ${
         scrollPosition > 10
           ? "bg-white shadow-md shadow-neutral-500/10"
           : ""
@@ -21,7 +21,7 @@ export default function Header() {
     >
       <div className="w-full flex items-center justify-between">
         <div className="flex items-center gap-x-2">
-          <button className="h-11 w-11 rounded-md bg-@sura-primary-900 grid place-items-center">
+          <button className="h-11 w-11 rounded-md bg-transparent border border-@sura-primary-600 grid place-items-center">
             <svg
               width="16"
               height="23"
@@ -31,7 +31,7 @@ export default function Header() {
             >
               <path
                 d="M14.6667 6.66667C14.6667 4.82533 13.92 3.15867 12.7147 1.95333C11.508 0.746667 9.84133 0 8 0C6.15867 0 4.492 0.746667 3.28533 1.95333C2.08 3.15867 1.33333 4.82533 1.33333 6.66667C1.33333 8.508 2.08 10.1747 3.28533 11.38C4.492 12.5867 6.15867 13.3333 8 13.3333C9.84133 13.3333 11.508 12.5867 12.7147 11.38C13.3345 10.7617 13.826 10.027 14.161 9.21811C14.496 8.40924 14.6678 7.54215 14.6667 6.66667ZM0 20C0 21.3333 3 22.6667 8 22.6667C12.6907 22.6667 16 21.3333 16 20C16 17.3333 12.8613 14.6667 8 14.6667C3 14.6667 0 17.3333 0 20Z"
-                fill="white"
+                fill="#303643"
               />
             </svg>
           </button>
