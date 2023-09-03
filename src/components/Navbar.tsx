1fcbaee028267191013838c5907109069ef9e589
Fernando Villalba <21fervillalbag@gmail.com>
2023-09-03 15:30:14 -0400

    fix: font size navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
   return (
     <div
-      className={`shadow-md shadow-@sura-primary-600/50 z-20 bottom-0 left-0 fixed w-full h-[64px] bg-@sura-primary-50 transition-opacity duration-300`}
+      className={`shadow-md shadow-@sura-primary-600/50 z-20 bottom-0 left-0 fixed w-full h-[68px] bg-@sura-primary-50 transition-opacity duration-300`}
     >
       <div className="shadow-md flex items-center h-full justify-between px-8">
         {NAVBAR_ROUTES.map((link) => (
