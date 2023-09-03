8bd1b96ad4b6eedad0d4df5b9a783ed2d062ff86
Fernando Villalba <21fervillalbag@gmail.com>
2023-09-03 17:08:10 -0400

    feat: navbar light

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
   return (
     <div
-      className={`shadow-md shadow-@sura-primary-700/80 z-20 bottom-0 left-0 fixed w-full h-[68px] rounded-tl-md rounded-tr-md bg-@sura-primary-100 transition-opacity duration-300`}
+      className={`shadow-2xl shadow-@sura-primary-900 z-20 bottom-0 left-0 fixed w-full h-[68px] bg-white transition-opacity duration-300`}
     >
       <div className="shadow-md flex items-center h-full justify-between px-7">
         {NAVBAR_ROUTES.map((link) => (
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,107 +1,36 @@
-// import React, { useContext, useEffect, useState } from "react";
 import React, { useContext, useEffect, useState } from "react";
 
 import {
-  InputHome,
-  // HeaderGallery,
-  // CardProduct,
   ModalLogin,
   PromotionSection,
   RecentsSection,
   SeasonsSection,
 } from "../components/Home";
 import { HeaderHome, Layout } from "../components";
-// import { Button, Text, buttonVariants } from "../ui";
-// import { AuthenticatedContext, WindowSizeContext } from "../context";
 import { AuthenticatedContext } from "../context";
 import { SURA_CREATE_POST_INFO } from "../utils/constants";
-// import { client } from "../../supabase/client";
-// import Line from "../components/Loader/Line";
-
-// import NotResultIcon from "../assets/images/not-result-icon.png";
-
-// const VALUE_AD = 3;
-
-// function compararPorTypeAd(a: any, b: any): number {
-//   if (a.typeAd === VALUE_AD && b.typeAd !== VALUE_AD) {
-//     return -1;
-//   } else if (a.typeAd !== VALUE_AD && b.typeAd === VALUE_AD) {
-//     return 1;
-//   } else {
-//     return 0;
-//   }
-// }
 
 const Home: React.FC = () => {
   const { isAuthenticated } = useContext(AuthenticatedContext);
 
-  // const [categorySelected, setCategorySelected] = useState<
-  //   number | null
-  // >(null);
-  // const { windowSize } = useContext(WindowSizeContext);
-
-  // const [products, setProducts] = useState<any | null>(null);
-  // const [errorProduct, setErrorProduct] = useState<any | null>(null);
-  // const [categories, setCategories] = useState<any | null>(null);
-  // const [errorCategories, setErrorCategories] = useState<any | null>(
-  //   null
-  // );
-
   const [showModalLogin, setShowModalLogin] = useState<boolean>(
     !isAuthenticated
   );
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const { data: products, error: errorProducts } = await client
-  //       .from("Product")
-  //       .select("id, images, title, price, currency, typeAd")
-  //       .eq(
-  //         categorySelected !== null ? "category" : "",
-  //         categorySelected
-  //       )
-  //       .order("created_at", { ascending: false });
-
-  //     if (errorProducts) {
-  //       console.log("Productos no encontrados");
-  //       return;
-  //     }
-  //     setErrorProduct(errorProducts);
-  //     setProducts(products);
-  //   })();
-  // }, [categorySelected]);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const { data: categories, error: errorCategories } =
-  //       await client
-  //         .from("CategoryProduct")
-  //         .select("*")
-  //         .order("created_at", { ascending: false });
-
-  //     if (errorCategories) {
-  //       console.log("Categoria no encontrados");
-  //       setErrorCategories(errorCategories);
-  //       return;
-  //     }
-
-  //     setCategories(categories);
-  //   })();
-  // }, []);
 
   useEffect(() => {
     localStorage.setItem(SURA_CREATE_POST_INFO, "");
     window.scrollTo(0, 0);
   }, []);
 
-  // products?.sort(compararPorTypeAd);
-
   return (
     <Layout>
       <ModalLogin show={showModalLogin} setShow={setShowModalLogin} />
 
       <HeaderHome />
-      <InputHome />
+
+      <div className="px-5 mt-1">
+        <img src="/images/banner-promo.png" alt="" />
+      </div>
 
       <main className="py-5 flex flex-col gap-y-5">
         <PromotionSection />
