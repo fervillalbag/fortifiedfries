e82d5ab82443582c7ed77f49ebb57d7c76093807
Fernando Villalba <21fervillalbag@gmail.com>
2023-09-13 00:50:30 -0400

    fix: loader skeleton home gap

--- src/components/Home/Loader.tsx
+++ src/components/Home/Loader.tsx
@@ -1,6 +1,6 @@
 export default function Loader() {
   return (
-    <div className="px-5 grid grid-cols-2 gap-5">
+    <div className="px-5 grid grid-cols-2 gap-x-3 gap-y-4">
       <div className="w-full h-[208px] bg-@sura-primary-200 rounded-md animate-pulse"></div>
       <div className="w-full h-[208px] bg-@sura-primary-200 rounded-md animate-pulse"></div>
       <div className="w-full h-[208px] bg-@sura-primary-200 rounded-md animate-pulse"></div>
