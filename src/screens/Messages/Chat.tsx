6135e904bf198508bbee95a4b2d7a5670c5b1823
Fernando Villalba <21fervillalbag@gmail.com>
2023-10-22 16:59:46 -0300

    feat: create message fn implemented

--- src/screens/Messages/Chat.tsx
+++ src/screens/Messages/Chat.tsx
@@ -10,6 +10,8 @@ import {
 import { useLocalStorageState } from "../../hooks";
 import { SURA_CREDENTIALS } from "../../utils/constants";
 import { useGetUser } from "../../hooks/user";
+import toast from "react-hot-toast";
+import { createMessage } from "../../services/message";
 
 export default function Chat() {
   const { id } = useParams();
@@ -19,6 +21,7 @@ export default function Chat() {
 
   const [messages, setMessages] = useState<any[]>([]);
   const [userReceiver, setUserReceiver] = useState<any>(null);
+  const [content, setContent] = useState<string>("");
 
   const queryMarkMessagesAsSeen = useMarkMessagesAsSeen(
     value.id,
@@ -43,6 +46,29 @@ export default function Chat() {
     queryContacts.refetch();
   }, []);
 
+  const handleCreateMessage = async () => {
+    if (!content) return;
+
+    const messageInfo = {
+      content,
+      receiver: id,
+      sender: value.id,
+    };
+
+    try {
+      const response = await createMessage(messageInfo);
+
+      if (response.status === 201) {
+        queryContacts.refetch();
+        queryAllMessages.refetch();
+        setContent("");
+      }
+    } catch (error: any) {
+      toast.error("Ha ocurrido un error");
+      throw new Error(error);
+    }
+  };
+
   return (
     <div>
       <BackBtn title={userReceiver?.fullname} />
@@ -60,6 +86,30 @@ export default function Chat() {
             {message.content}
           </div>
         ))}
+      </div>
+
+      <div className="w-[calc(100%_-_40px)] fixed bottom-[25px] left-[20px]">
+        <input
+          value={content}
+          placeholder="Escribir mensaje"
+          className="outline-none focus-visible:ring-1 ring-@sura-primary-900 h-[54px] rounded-md border border-@sura-primary-200 w-full pl-4 pr-[46px]"
+          onChange={(e) => setContent(e.target.value)}
+        />
+
+        <button
+          className="bg-white/60 outline-none absolute top-1/2 rounded-md -translate-y-1/2 right-[1px] py-0 h-[calc(100%_-_2px)] px-3 pl-4"
+          onClick={handleCreateMessage}
+        >
+          <svg
+            width="19"
+            height="16"
+            viewBox="0 0 19 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 16V10L8 8L0 6V0L19 8L0 16Z" fill="#1C2331" />
+          </svg>
+        </button>
       </div>
     </div>
   );
